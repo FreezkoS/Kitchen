85c0bd4eb5576b73841b97fc6d2eeb08e53c0fcd
Egor <egor.uxanov.97@mail.ru>
2017-11-11 04:11:03 -0500

    sfsfsf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://dashboard.heroku.com/apps/frozen-retreat-75779'
+  # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -67,7 +67,7 @@ config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
+config.action_mailer.default_url_options = { :host => 'https://frozen-retreat-75779.herokuapp.com/' }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
