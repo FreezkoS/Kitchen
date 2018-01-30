e5a41615a8d32250009c7a23395a0c40b25acce5
Egor <egor.uxanov.97@mail.ru>
2018-01-30 04:41:44 -0500

    sfsf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-
+ config.action_mailer.delivery_method = :sendmail
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
