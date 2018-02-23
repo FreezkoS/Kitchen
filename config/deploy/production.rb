b6c3d0705371cdf8fa28bee56a6c03cd20efb2c2
Egor <egor.uxanov.97@mail.ru>
2018-02-23 04:19:14 -0500

    fgfdg

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "kingkitchen.pro", user: 'deploy', roles: %w{app db web}
+server "188.166.99.60", user: 'deploy', roles: %w{app db web}
 
 
 
