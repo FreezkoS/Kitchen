ea235e70274693e0f3fc1a673c9fb529ab7ee35f
Egor <egor.uxanov.97@mail.ru>
2018-05-28 01:05:46 -0400

    dsds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require_tree .
 //= require bootstrap-sprockets
 //= require jquery-ui
+//= require jquery-ui/effect.all
--- app/controllers/zams_controller.rb
+++ app/controllers/zams_controller.rb
@@ -4,4 +4,8 @@ class ZamsController < ApplicationController
       ZamsMailer.zayavka(@clue).deliver
       redirect_to root_path, success: 'Запрос успешно отправлен!'
   end
+    include ActiveModel::Validations
+
+  validates :email, presence: true
+  validates :name, presence: true
 end
