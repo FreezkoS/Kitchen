37b4edc51e49689ad7f554be413696f10c3cef70
Egor <egor.uxanov.97@mail.ru>
2018-05-28 01:49:27 -0400

    dsds

--- app/controllers/zams_controller.rb
+++ app/controllers/zams_controller.rb
@@ -4,8 +4,4 @@ class ZamsController < ApplicationController
       ZamsMailer.zayavka(@clue).deliver
       redirect_to root_path, success: 'Запрос успешно отправлен!'
   end
-    include ActiveModel::Validations
-
-  validates :email, presence: true
-  validates :name, presence: true
 end
